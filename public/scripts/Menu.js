7152d9e5976a9736f2488ee4b8c21de5a8da1d5a
Nick Lyons <telagraphic@gmail.com>
2025-06-13 11:41:40 -0400

    Setup menu component

---
+++ public/scripts/Menu.js
@@ -0,0 +1,15 @@
+class Menu {
+  constructor() {
+    this.menu = document.querySelector(".menu");
+    this.menuButton = document.querySelector(".menu__button");
+  }
+
+
+  /**
+   * Listen to keyboard event for "M" key to toggle the menu
+   * Toggle function for hiding/showing the menu
+   * Animate the menu in/out
+   */
+}
+
+export default Menu;
